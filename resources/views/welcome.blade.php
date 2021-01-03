e9023362509d11ea21b4b6fd9bdd889c6686f7dc
prosper <prosper@lunasoft.co.za>
2021-01-03 23:06:29 +0200

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
@@ -66,6 +65,4 @@
 	<script src="assets/js/admin.js"></script>
 
 </body>
-
-
 </html>
